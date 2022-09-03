824eb0a37f0ea00c72aa6288112430b6b18ece5b
minsuKang <90169703+minchodang@users.noreply.github.com>
2022-09-03 15:20:20 +0900

    Feat: 동적경로 & 라우트 추가 (#5)

---
+++ pages/portfolio/[projectid].tsx
@@ -0,0 +1,11 @@
+import { useRouter } from 'next/router';
+
+export const PortfolioProjectPage = () => {
+    const router = useRouter();
+
+    return (
+        <div>
+            <h1>The Portfolio Project Page</h1>
+        </div>
+    );
+};
