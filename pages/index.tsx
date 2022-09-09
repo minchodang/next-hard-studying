886094696682f8e05904236df5475c10df8c0c95
minchodang <minsu910725@gmail.com>
2022-09-09 18:06:27 +0900

    Feat: catchAll 라우트 추가, Link 동적 라우팅

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,24 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <h1>Hell next js.</h1>
-    </div>
-  )
-}
+    return (
+        <div className={styles.container}>
+            <h1>The Home Page</h1>
+            <ul>
+                <li>
+                    <Link href={'/portfolio'}>Portfolio</Link>
+                </li>
+                <li>
+                    <Link href={'/clients'}>Clients</Link>
+                </li>
+                <li></li>
+            </ul>
+        </div>
+    );
+};
 
-export default Home
+export default Home;
