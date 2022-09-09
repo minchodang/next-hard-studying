61385f0c60e72e76a020a446f7221dc4a5c5342b
minsuKang <90169703+minchodang@users.noreply.github.com>
2022-09-09 18:25:58 +0900

    Develop (#13)
    * Feat: 초기 셋팅 및 기본 index 수정 (#1)
    
    * Feat: 라우팅 파일 페이지 추가 (#3)
    
    * Feat: 동적경로 & 라우트 추가 (#5)
    
    * Feature/dynamic page routing2 (#7)
    
    * Feat: 동적경로 & 라우트 추가
    
    * Feat: 이그노어 수정
    
    * ignore 수정 및 추가
    
    * 중첩 라우트 & 경로 구축 (#10)
    
    * Feat: catchAll 라우트 추가, Link 동적 라우팅 (#12)

--- pages/clients/index.tsx
+++ pages/clients/index.tsx
@@ -1,8 +1,26 @@
+import Link from 'next/link';
+
 const ClientPage = () => {
+    const clients = [
+        {
+            id: 'max',
+            name: 'Maximilian',
+        },
+        {
+            id: 'manu',
+            name: 'Manuel',
+        },
+    ];
     return (
         <div>
             <h1>The Clients Page</h1>
-        </div>
+            <ul>
+                {clients.map((client) => (
+                    <li key={client.id}>
+                        <Link href={`/clients/${client.id}`}>{client.name}</Link>
+                    </li>
+                ))}
+            </ul>
     );
 };
 
