afdb4d499f8af0a7dffa4c00aebc14e4734b9256
minsuKang <90169703+minchodang@users.noreply.github.com>
2022-09-10 16:15:07 +0900

    Feat: 동적라우팅 네비게이팅, 커스텀 404 (#14)

--- pages/clients/[id]/index.tsx
+++ pages/clients/[id]/index.tsx
@@ -1,7 +1,20 @@
+import { router } from 'next/client';
+
 const ClientProjectsPage = () => {
+    const loadProjectHandler = () => {
+        router.push({
+            pathname: '/clients/[id]/[clientprojectid]',
+            query: {
+                id: 'max',
+                clientprojectid: 'projecta',
+            },
+        });
+    };
+
     return (
         <div>
             <h1>THE PROJECTS OF A GIVEN CLIENT</h1>
+            <button onClick={loadProjectHandler}>Load Project A</button>
         </div>
     );
 };
